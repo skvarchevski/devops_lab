0ad1d066c62111c61339463705a948edf73c7b1c
skvarchevski <skvarchevski@localhost.localdomain>
2020-03-09 00:13:45 +0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="snapshot",
+    packages=setuptools.find_packages(),
+    scripts=['snapshot'],
+    version="1.0",
+    author="Aliaksandr Skvarcheuski",
+    author_email="aliaksandr_skvarcheuski@epam.com",
+    description="Simple system snapshots"
+)
